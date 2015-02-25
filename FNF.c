e0e140c9fcce6f865bcdcfdb8444f7ea7fb83d58
John Tramm <john.tramm@gmail.com>
2015-02-24 19:02:02 -0500

    added alert about c99

--- FNF.c
+++ FNF.c
@@ -21,7 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-#include<complex.h>
+#include<complex.h> // C99 only! Must be compiled with -std=c99 or -std=gnu99
 
 double complex FNF( double complex Z )
 {
